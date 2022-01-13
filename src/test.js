a63e8e0dee306932fa9f3ccf318ca450e4b99e40
Djuza, Dragan <dragan.djuza@centogene.com>
2022-01-13 14:05:40 +0100

    instaled es lint, prettier and typescript

---
+++ src/test.js
@@ -0,0 +1,8 @@
+var a;
+var b;
+var c;
+var user = {
+    name: 'sdfsdfdsfs',
+    surname: 'dgfdhgfhgfhf',
+    age: 34
+};
---
+++ src/test.ts
@@ -0,0 +1,16 @@
+let a:number;
+let b:boolean;
+let c:string;
+
+type User = {
+    name: string;
+    surname: string;
+    age:number;
+} 
+
+const user:User = {
+    name:'sdfsdfdsfs',
+    surname:'dgfdhgfhgfhf',
+    age:34
+};
+
